2e7ba2f4fc6addf3c27fe3118071196a1dab70f4
Reynlldi <reynalddi93@gmail.com>
2022-08-12 14:12:58 +0700

    Memperbaiki dropdown

--- dashboardMahasiswa.php
+++ dashboardMahasiswa.php
@@ -40,9 +40,9 @@ require "inc.koneksi.php";
       <i class='bx bx-menu' id="header-toggle"></i>
     </div>
     <div class="dropdown ">
-      <a class="btn dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown"
-        aria-expanded="false">
-        <?php echo 'Welcome, <strong>' . $_SESSION["nama"] . '</strong>' ?>
+      <a class="dropdown-toggle text-dark text-decoration-none" href="#" role="button" id="dropdownMenuButton1"
+        data-bs-toggle="dropdown" aria-expanded="true">
+        <?php echo 'Selamat Datang, <strong>' . $_SESSION["nama"] . '</strong>' ?>
       </a>
       <hr>
       <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
